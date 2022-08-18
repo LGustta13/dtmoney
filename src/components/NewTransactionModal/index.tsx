1283aece7ddfe81a35bc847df3fcd4490fd91f9f
Luis Gustavo <luisgustavoolivtp@hotmail.com>
2022-08-18 00:20:39 -0300

    Adding buttons for income and outcome

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,9 @@
 import Modal from 'react-modal';
-import { Container } from './styles';
+import { Container, TransactionTypeContainer} from './styles';
 import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+
 
 type NewTransactionModalProps = {
     isOpen: boolean,
@@ -34,6 +37,22 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
                 type="number"
                 placeholder="Valor"
             />
+
+            <TransactionTypeContainer>
+                <button
+                    type="button"
+                >
+                    <img src={incomeImg} alt="Entrada"/>
+                    <span>Entrada</span>
+                </button>
+
+                <button
+                    type="button"
+                >
+                    <img src={outcomeImg} alt="Saída"/>
+                    <span>Saída</span>
+                </button>
+            </TransactionTypeContainer>
 
             <input
                 placeholder="Categoria"
