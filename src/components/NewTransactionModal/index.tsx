107717912642042cc2461ea38fce4c19218f528d
Luis Gustavo <luisgustavoolivtp@hotmail.com>
2022-08-22 00:19:58 -0300

    Finishing the modal api for inserting data

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,7 +3,8 @@ import { Container, TransactionTypeContainer, RadioBox} from './styles';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
+import { api } from '../../services/api';
 
 
 type NewTransactionModalProps = {
@@ -13,6 +14,9 @@ type NewTransactionModalProps = {
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
 
+    const [title, setTitle] = useState('');
+    const [value, setValue] = useState(0);
+    const [category, setCategory] = useState('');
     const [type, setType] = useState('deposit');
 
     function handleSetTypeDeposit(){
@@ -21,6 +25,19 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
 
     function handleSetTypeWithdraw(){
         setType('withdraw');
+    }
+
+    function handleCreateNewTransaction(event: FormEvent){
+        event.preventDefault(); 
+
+        const data = {
+            title,
+            value,
+            category,
+            type
+        };
+
+        api.post('/transactions', data)
     }
 
     return (
@@ -38,16 +55,20 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
             <img src={closeImg} alt="Fechar modal"/>
         </button>
 
-        <Container>
+        <Container onSubmit={handleCreateNewTransaction}>
             <h2>Cadastrar transação</h2>
 
             <input
                 placeholder="Título"
+                value={title}
+                onChange={event => setTitle(event.target.value)}
             />
 
             <input
                 type="number"
                 placeholder="Valor"
+                value={value}
+                onChange={event => setValue(Number(event.target.value))}
             />
 
             <TransactionTypeContainer>
@@ -74,6 +95,8 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
 
             <input
                 placeholder="Categoria"
+                value={category}
+                onChange={event => setCategory(event.target.value)}
             />
 
             <button type="submit">
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,46 @@
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {App} from './App';
 
 createServer({
+  models: {   // Crio um banco de dados com uma tabela transaction
+    transaction: Model,
+  },
+
+  // Carregar alguns dados iniciais
+  seeds(server) {server.db.loadData({
+    transactions: [
+      {
+        id: 1,
+        title: 'Freelancer de website',
+        type: 'deposit',
+        category: 'Dev',
+        amount: 6000,
+        createdAt: new Date('2021-02-12 09:00:00'),
+      },
+      {
+        id: 2,
+        title: 'Aluguel',
+        type: 'withdraw',
+        category: 'Casa',
+        amount: 1100,
+        createdAt: new Date('2021-02-14 11:00:00'),
+      }
+    ],
+  }
+)},
+
   routes() {
     this.namespace = 'api';
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ];
+      return this.schema.all('transaction')
+    });
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody); //JSON.parse para transformar texto em JSON
+
+      return schema.create('transaction', data);
     });
   }
 })
